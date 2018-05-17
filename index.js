ff78cf3baac3aa5ccc0bef1a50ecb1d411a71e1c
Gergő Móricz <mo.geryy@gmail.com>
2018-05-17 12:00:25 +0200

    Removed JSDoc: coming back soon!

--- index.js
+++ index.js
@@ -1,33 +1,5 @@
 var Discord = require('discord.js')
 
-/**
- * This callback handles messages from Discord.
- * @callback messageCallback
- * @param {string[]} matches The matches of the message's regex.
- * @param {Discord.Message} message The message object from Discord.js
- */
-
-/**
- * Command object used internally.
- * @typedef {Object} command
- * @property {RegExp} regex The command's regex.
- * @property {messageCallback} action The command's callback.
- */
-
-/**
- * Main discwork objext returned by init.
- * @typedef {Object} discwork
- * @property {function} add
- * @property {function} done
- * @property {command} commands
- * @property {Discord.Client} client
- */
-
-/**
- * Add a new command.
- * @param {RegExp} regex The command's regex.
- * @callback {messageCallback} The callback that handles the message.
- */
 function add (regex, action) {
   if (!(regex instanceof RegExp)) {
     throw new Error('Passed regex is not an instance of RegExp.')
@@ -39,9 +11,6 @@ function add (regex, action) {
   })
 }
 
-/**
- * Finish command list.
- */
 function done () {
   this.client.on('message', function (message) {
     this.commands.some(function (cmd) {
@@ -54,11 +23,6 @@ function done () {
   }.bind(this))
 }
 
-/**
- * Initializes a discwork instance.
- * @param {Discord.Client} client Your discord.js client.
- * @returns {discwork} The main discwork object.
- */
 function init (client) {
   if (!(client instanceof Discord.Client)) {
     throw new Error('The client needs to be a discord.js client.')
